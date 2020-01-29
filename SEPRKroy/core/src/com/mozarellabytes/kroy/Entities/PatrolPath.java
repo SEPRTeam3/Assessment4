e68ff91b938265ee4e6d30f041daf7ea27d39642
jakeh <jacobwbillington@gmail.com>
2020-01-29 12:12:03 +0000

    Alien and patrol path empty classes added

---
+++ SEPRKroy/core/src/com/mozarellabytes/kroy/Entities/PatrolPath.java
@@ -0,0 +1,10 @@
+package com.mozarellabytes.kroy.Entities;
+
+public class PatrolPath {
+    /**
+     * PatrolPath class contains hard coded patrols for each alien
+     * Hardcoded paths are queues of vector2's handled in the same way as for firetruck
+     * Methods to decide which patrol list to return to the given alien (takes alien as input, returns path)
+     * MAYBE contains methods to handle collisions with another alien and changing patrol and directions
+     **/
+}
