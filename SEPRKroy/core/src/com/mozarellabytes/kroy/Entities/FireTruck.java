a3ea2f8cacce3f7d1dc3c716056f451db02238ba
lam589 <lam589@york.ac.uk>
2020-01-31 15:19:18 +0000

    Fixed y axis bug

--- SEPRKroy/core/src/com/mozarellabytes/kroy/Entities/FireTruck.java
+++ SEPRKroy/core/src/com/mozarellabytes/kroy/Entities/FireTruck.java
@@ -187,7 +187,7 @@ public class FireTruck extends Sprite {
      *                      <code>false</code> otherwise
      */
     private boolean isValidDraw(Vector2 coordinate) {
-        if (coordinate.y < 24) {
+        if (coordinate.y < 30) {
             if (gameScreen.isRoad((Math.round(coordinate.x)), (Math.round(coordinate.y)))) {
                 if (this.path.isEmpty()) {
                     return this.getPosition().equals(coordinate);
