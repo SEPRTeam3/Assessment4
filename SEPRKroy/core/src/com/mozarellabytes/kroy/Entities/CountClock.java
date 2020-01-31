93d59f737cce8738348dc28dbb9702d0b085025f
SHOUYI YUAN <AFSysy42927223>
2020-01-31 19:04:37 +0000

    Merge branch 'master' of https://github.com/silverben10/kroy-assessment3
     Conflicts:
    	.idea/jarRepositories.xml
    	.idea/misc.xml
    	.idea/workspace.xml

--- SEPRKroy/core/src/com/mozarellabytes/kroy/Entities/CountClock.java
+++ SEPRKroy/core/src/com/mozarellabytes/kroy/Entities/CountClock.java
@@ -12,18 +12,41 @@ public class CountClock {
     Constructor
      */
     public CountClock() {
-        remainTime = 5*60;
+        remainTime = 65;
     }
 
     /*
     Set remaining time, with counting down, state 0 means counting and 1 means pause
     */
-    public String set_clock(int state){
-        if(state == 0) {
+    public String set_clock(int state) {
+
+        if (state == 0) {
             remainTime -= Gdx.graphics.getDeltaTime();
         }
 
-        return (int)remainTime/60+":"+(int)remainTime%60;
+        if(remainTime / 60 >= 10 && remainTime % 60 < 10 && remainTime >= 0){
+            return (int) remainTime / 60 + " : 0" + (int) remainTime % 60;
+        }
+
+        else if(remainTime / 60 >= 10 && remainTime % 60 >= 10 && remainTime >= 0){
+            return (int) remainTime / 60 + " : " + (int) remainTime % 60;
+        }
+
+        else if(remainTime / 60 <= 10 && remainTime % 60 >=10 && remainTime >= 0){
+            return "0" + (int) remainTime / 60 + " : " + (int) remainTime % 60;
+        }
+
+        else if(remainTime / 60 <= 10 && remainTime % 60 <10 && remainTime >= 0){
+            return "0" + (int) remainTime / 60 + " : 0" + (int) remainTime % 60;
+        }
+
+        else if(remainTime < 0){
+            return "00 : 00";
+        }
+
+        else {
+            return "0" + (int) remainTime / 60 + " : " + (int) remainTime % 60;
+        }
     }
 }
 
