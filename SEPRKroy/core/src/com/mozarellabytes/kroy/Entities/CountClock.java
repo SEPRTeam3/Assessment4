903405697b50020b24f7d0c258f4382d4767357a
Anthony <antspartanelite@gmail.com>
2020-01-31 18:32:47 +0000

    Fixed clock (almost still need to account for size changes)

--- SEPRKroy/core/src/com/mozarellabytes/kroy/Entities/CountClock.java
+++ SEPRKroy/core/src/com/mozarellabytes/kroy/Entities/CountClock.java
@@ -1,5 +1,6 @@
 package com.mozarellabytes.kroy.Entities;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.utils.TimeUtils;
 
 import java.awt.*;
@@ -8,32 +9,13 @@ public class CountClock {
     /*
     seconds and millisecond in the countdown Clock
      */
-    private int seconds, millisecond;
-    private long startTime;
-    private long pauseTime;
-    private String remainTime;
-    private static int x,y,flag;
+    private float remainTime;
 
     /*
     Constructor
      */
     public CountClock() {
-        startTime = TimeUtils.millis();
-        pauseTime = 0;
-        seconds = 200;
-        millisecond = 0;
-        x = 750;
-        y = 1030;
-    }
-
-
-    /*
-    Initialise time and clock position
-     */
-    static {
-        x = 750;
-        y = 1030;
-        flag = 0;
+        remainTime = 5*60;
     }
 
     /*
@@ -41,20 +23,10 @@ public class CountClock {
     */
     public String set_clock(int state){
         if(state == 0) {
-            if(pauseTime != 0){
-                startTime += pauseTime - startTime;
-                pauseTime = 0;
-            }
-            } else {
-                pauseTime = TimeUtils.millis();
-            }
+            remainTime -= Gdx.graphics.getDeltaTime();
+        }
 
-
-        /*
-        Get the remaining time and return it
-         */
-        remainTime = (seconds - TimeUtils.timeSinceMillis(startTime)/1000) +":"+ (TimeUtils.timeSinceMillis(startTime) - (TimeUtils.timeSinceMillis(startTime)/1000)*1000);
-        return remainTime;
+        return (int)remainTime/60+":"+(int)remainTime%60;
     }
 }
 
