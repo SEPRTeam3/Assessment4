b4e6b7eb65ee54e3d71a585842fb11dfd15d2143
SHOUYI YUAN <AFSysy42927223>
2020-01-31 18:34:03 +0000

    Merge branch 'master' of https://github.com/silverben10/kroy-assessment3
     Conflicts:
    	.idea/jarRepositories.xml
    	.idea/misc.xml
    	.idea/workspace.xml

--- SEPRKroy/core/src/com/mozarellabytes/kroy/Entities/CountClock.java
+++ SEPRKroy/core/src/com/mozarellabytes/kroy/Entities/CountClock.java
@@ -11,8 +11,10 @@ public class CountClock {
     private int seconds, millisecond;
     private long startTime;
     private long pauseTime;
+    private long usedTime;
     private String remainTime;
     private static int x,y,flag;
+    private long last_pause_time = 0, last_start_time = 0;
 
     /*
     Constructor
@@ -39,16 +41,24 @@ public class CountClock {
     /*
     Set remaining time, with counting down, state 0 means counting and 1 means pause
     */
-    public String set_clock(int state){
-        if(state == 0) {
-            if(pauseTime != 0){
-                startTime += pauseTime - startTime;
-                pauseTime = 0;
-            }
-            } else {
-                pauseTime = TimeUtils.millis();
-            }
+    public String set_clock(int state) {
 
+        if (state == 0) {    //playing
+            pauseTime = TimeUtils.millis();
+            last_start_time = startTime;
+            if (pauseTime != 0) { //pause has been pressed
+                last_pause_time = pauseTime;
+                System.out.print(last_pause_time);
+                last_pause_time = pauseTime - startTime;
+                System.out.print(99999999);
+                startTime += pauseTime - startTime; //pauseTime - startTime -> How long has been played
+            }
+        }
+
+        if(state == 1) {        // pause
+            System.out.print("aaaaaaaaaaaaaa");
+            startTime = last_pause_time - last_start_time;
+        }
 
         /*
         Get the remaining time and return it
