d0df7431fbf11c8213db540028608c4c03ddcf3a
Ben Silverman <bensilverman10@gmail.com>
2020-02-03 13:10:50 +0000

    Added droplet images and updated game logic

--- SEPRKroy/core/src/com/mozarellabytes/kroy/Screens/MinigameScreen.java
+++ SEPRKroy/core/src/com/mozarellabytes/kroy/Screens/MinigameScreen.java
@@ -16,6 +16,7 @@ import com.badlogic.gdx.utils.TimeUtils;
 import com.mozarellabytes.kroy.Kroy;
 import com.mozarellabytes.kroy.Utilities.Constants;
 
+import java.sql.Time;
 import java.util.Iterator;
 
 
@@ -49,9 +50,20 @@ public class MinigameScreen implements Screen {
     /** Rectangle for controlling the firetruck */
     private Rectangle fireTruck;
 
+    /** Array to keep track of all aliens */
     private Array<Rectangle> aliens;
 
+    /** Keeping track of the time since the last alien was spawned */
     private long lastAlienSpawn;
+
+    /** Image for water droplet */
+    private Texture dropletImage;
+
+    /** Array to keep track of all water droplets */
+    private Array<Rectangle> droplets;
+
+    /** Keeping track of the last time a water droplet was shot */
+    private long lastDropTime = 0;
 
     /**
      * Constructor to instantiate all the assets and entities.
@@ -64,6 +76,8 @@ public class MinigameScreen implements Screen {
         truckImage = truckLeft;
 
         alienImage = new Texture(Gdx.files.internal("sprites/alien/alien.png"));
+
+        dropletImage = new Texture(Gdx.files.internal("sprites/droplet/droplet180.png"));
 
         waterSound = Gdx.audio.newSound(Gdx.files.internal("sounds/sfx/truck_attack.wav"));
 
@@ -80,6 +94,8 @@ public class MinigameScreen implements Screen {
 
         aliens = new Array<Rectangle>();
         spawnAlien();
+
+        droplets = new Array<Rectangle>();
     }
 
     @Override
@@ -99,6 +115,10 @@ public class MinigameScreen implements Screen {
         for (Rectangle alien: aliens) {
             batch.draw(alienImage, alien.x, alien.y);
         }
+
+        for (Rectangle droplet: droplets) {
+            batch.draw(dropletImage, droplet.x, droplet.y);
+        }
         batch.end();
 
         this.update(delta);
@@ -107,40 +127,67 @@ public class MinigameScreen implements Screen {
     /**
      * Handle controls and minigame logic.
      *
-     * @param delta The time in millisecond since the last render
+     * @param delta The time in milliseconds since the last render
      * */
     public void update(float delta) {
+
+//      Fire truck controls
         if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-            fireTruck.x -= 500 * Gdx.graphics.getDeltaTime();
+            fireTruck.x -= 500 * delta;
             truckImage = truckLeft;
         }
 
         if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-            fireTruck.x += 500 * Gdx.graphics.getDeltaTime();
+            fireTruck.x += 500 * delta;
             truckImage = truckRight;
         }
 
+        if (Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
+            // Shoot water droplet upwards
+            if (TimeUtils.nanoTime() - lastDropTime > 50000000 || lastDropTime == 0) {
+                shootDroplet();
+            }
+        }
+
+//      Stop fire trucks moving out of bounds
         if (fireTruck.x < 0) {
             fireTruck.x = 0;
         }
 
-        if (fireTruck.x > Constants.GAME_WIDTH) {
-            fireTruck.x = Constants.GAME_WIDTH;
+        if (fireTruck.x > Constants.GAME_WIDTH  - 64) {
+            fireTruck.x = Constants.GAME_WIDTH - 64;
         }
 
         if (TimeUtils.nanoTime() - lastAlienSpawn > 1000000000) {
             spawnAlien();
         }
 
+        // Alien movement and logic
         for (Iterator<Rectangle> iter = aliens.iterator(); iter.hasNext();) {
             Rectangle alien = iter.next();
-            alien.y -= 150 * Gdx.graphics.getDeltaTime();
+            alien.y -= 150 * delta;
 
             if (alien.y + 64 < 0) {
                 iter.remove();
             }
 
-            if (alien.overlaps(fireTruck)) {
+//            if (alien.overlaps(fireTruck)) {
+//                iter.remove();
+//            }
+
+            for (Rectangle droplet: droplets) {
+                if (alien.overlaps(droplet)) {
+                    iter.remove();
+                }
+            }
+        }
+
+        // Droplet movement and logic
+        for (Iterator<Rectangle> iter = droplets.iterator(); iter.hasNext();) {
+            Rectangle droplet = iter.next();
+            droplet.y += 300 * delta;
+
+            if (droplet.y > Constants.GAME_HEIGHT) {
                 iter.remove();
             }
         }
@@ -178,6 +225,16 @@ public class MinigameScreen implements Screen {
         alien.height = 64;
         aliens.add(alien);
         lastAlienSpawn = TimeUtils.nanoTime();
+    }
+
+    private void shootDroplet() {
+        Rectangle droplet = new Rectangle();
+        droplet.x = fireTruck.x;
+        droplet.y = fireTruck.y + 64;
+        droplet.width = 32;
+        droplet.height = 32;
+        droplets.add(droplet);
+        lastDropTime = TimeUtils.nanoTime();
     }
 
     @Override
