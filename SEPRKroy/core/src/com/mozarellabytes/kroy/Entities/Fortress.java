54f126a7f3425a8e3d6dfce37c683480d59f81a2
Anthony <antspartanelite@gmail.com>
2020-02-12 12:36:34 +0000

    Fixed fortresses leveling up

--- SEPRKroy/core/src/com/mozarellabytes/kroy/Entities/Fortress.java
+++ SEPRKroy/core/src/com/mozarellabytes/kroy/Entities/Fortress.java
@@ -83,8 +83,8 @@ public class Fortress {
 
     //TODO add test
     public int getLevel() {
-        if(TimeUtils.timeSinceMillis(timeSinceLevelUp)/120000 >= 1 && level < 5) {
-            level = Math.min(level + (int)TimeUtils.timeSinceMillis(timeSinceLevelUp)/12000, 5);
+        if(TimeUtils.timeSinceMillis(timeSinceLevelUp)/60000 >= 1 && level < 5) {
+            level = Math.min(level + (int)(TimeUtils.timeSinceMillis(timeSinceLevelUp)/60000), 5);
             timeSinceLevelUp = TimeUtils.millis();
         }
         return level;
