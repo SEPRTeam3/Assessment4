cc73720fdf902ecf4e46c649152a8f938ace953d
SHOUYI YUAN <afsyuanshouyi@outlook.com>
2020-02-15 23:04:34 -0800

    Doubled the game music, change the hp of fortress and change the alien AP

--- SEPRKroy/core/src/com/mozarellabytes/kroy/Entities/EnemyAttackHandler.java
+++ SEPRKroy/core/src/com/mozarellabytes/kroy/Entities/EnemyAttackHandler.java
@@ -58,7 +58,7 @@ public class EnemyAttackHandler {
     EnemyAttackHandler(Alien entity){
         position = entity.getPosition();
         attackRange = ThreadLocalRandom.current().nextInt(2, 4);
-        attackPower = 0.18f;
+        attackPower = 0.4f;
         attackLevel = 1;
         delay = 100;
         bombs = new ArrayList<Bomb>();
--- SEPRKroy/core/src/com/mozarellabytes/kroy/Entities/FortressType.java
+++ SEPRKroy/core/src/com/mozarellabytes/kroy/Entities/FortressType.java
@@ -19,14 +19,14 @@ public enum FortressType {
      * attack points, width, height and sprite.
      */
     //#Asessment3 Changed attack power values for assessment 3
-    Revs ("Revolution", 2500, 7, 60, 4, 5, 3, new Texture(Gdx.files.internal("sprites/fortress/fortress_revs.png"))),
-    Walmgate ("Walmgate Bar", 1500, 8, 100, 6, 5, 5, new Texture(Gdx.files.internal("sprites/fortress/fortress_walmgate.png"))),
-    Clifford ("Clifford's Tower", 500, 4, 75, 8, 4, 3, new Texture(Gdx.files.internal("sprites/fortress/fortress_clifford.png"))),
+    Revs ("Revolution", 2500, 7, 90, 4, 5, 3, new Texture(Gdx.files.internal("sprites/fortress/fortress_revs.png"))),
+    Walmgate ("Walmgate Bar", 1500, 8, 120, 6, 5, 5, new Texture(Gdx.files.internal("sprites/fortress/fortress_walmgate.png"))),
+    Clifford ("Clifford's Tower", 500, 4, 120, 8, 4, 3, new Texture(Gdx.files.internal("sprites/fortress/fortress_clifford.png"))),
 
     //#Assessment3 Added 3 new fortresses
-    TrainStation ("Train Station", 3000, 5, 60, 5, 7, 4, new Texture(Gdx.files.internal("sprites/fortress/train.png"))),
-    Minster ("Minster", 2000, 6, 100, 7, 5, 6, new Texture(Gdx.files.internal("sprites/fortress/minster.png"))),
-    Shambles ("Shambles", 1000, 9, 40, 6, 6, 4, new Texture(Gdx.files.internal("sprites/fortress/shambles.png")));
+    TrainStation ("Train Station", 3000, 5, 90, 5, 7, 4, new Texture(Gdx.files.internal("sprites/fortress/train.png"))),
+    Minster ("Minster", 2000, 6, 110, 7, 5, 6, new Texture(Gdx.files.internal("sprites/fortress/minster.png"))),
+    Shambles ("Shambles", 1000, 9, 80, 6, 6, 4, new Texture(Gdx.files.internal("sprites/fortress/shambles.png")));
 
     /** The name for the fortress, visible once the fortress has been clicked on */
     private final String name;
