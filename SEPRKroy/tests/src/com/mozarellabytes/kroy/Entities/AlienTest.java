94e828842c8e79a8f638916d71553be8a0efe342
superev12 <evan.snoswell@gmail.com>
2020-04-28 15:10:30 +0100

    Rewrote Alien tests to work with new Alien ai.

--- SEPRKroy/tests/src/com/mozarellabytes/kroy/Entities/AlienTest.java
+++ SEPRKroy/tests/src/com/mozarellabytes/kroy/Entities/AlienTest.java
@@ -1,4 +1,3 @@
-/*
 package com.mozarellabytes.kroy.Entities;
 
 import com.badlogic.gdx.math.Vector2;
@@ -6,108 +5,97 @@ import com.badlogic.gdx.utils.Queue;
 import com.mozarellabytes.kroy.GameState;
 import com.mozarellabytes.kroy.GdxTestRunner;
 import com.mozarellabytes.kroy.Screens.GameScreen;
+import com.mozarellabytes.kroy.Utilities.PathFinder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 
-import static com.mozarellabytes.kroy.Entities.FireTruckType.*;
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @RunWith(GdxTestRunner.class)
 public class AlienTest {
 
     @Mock
-    GameScreen gameScreenMock;
-
-    @Mock
-    GameState gameStateMock;
+    GameScreen gameScreenMock = mock(GameScreen.class);
 
     @Test
     public void alienMoveRightTest() {
-        Queue<Vector2> vectorQueue = new Queue<>();
-        vectorQueue.addFirst(new Vector2(1, 0));
+        when(gameScreenMock.isRoad(anyInt(), anyInt())).thenReturn(true);
+        List<Vector2> vectorList = new ArrayList<>();
+        vectorList.add(new Vector2(4, 1));  //Waypoint to move towards
+        GameState gameState = new GameState();
+        Fortress masterFortress = new Fortress(0f, 0f, FortressType.Clifford, gameState);
 
-        ArrayList<FireTruck> truckArrayList = new ArrayList<>();
+        Alien alien = new Alien(1f, 1f, vectorList, 1f, masterFortress, new PathFinder(gameScreenMock), gameState);
+        alien.move(1f, new ArrayList<FireTruck>());
 
-        Alien alien = new Alien(0, 0, gameStateMock);
-
-        alien.move(truckArrayList);
-
-        assertEquals(new Vector2(1, 0), alien.getPosition());
+        assertEquals(new Vector2(2, 1), alien.getPosition());
     }
 
     @Test
     public void alienMoveLeftTest() {
-        Queue<Vector2> vectorQueue = new Queue<>();
-        vectorQueue.addFirst(new Vector2(-1, 0));
+        when(gameScreenMock.isRoad(anyInt(), anyInt())).thenReturn(true);
+        List<Vector2> vectorList = new ArrayList<>();
+        vectorList.add(new Vector2(0, 1));     //Waypoint to move towards
+        GameState gameState = new GameState();
+        Fortress masterFortress = new Fortress(0f, 0f, FortressType.Clifford, gameState);
 
-        ArrayList<FireTruck> truckArrayList = new ArrayList<>();
-
-        Alien alien = new Alien(0, 0, vectorQueue, 1);
-
-        alien.move(truckArrayList);
-
-        assertEquals(new Vector2(-1, 0), alien.getPosition());
-    }
-
-    @Test
-    public void alienMoveUpTest() {
-        Queue<Vector2> vectorQueue = new Queue<>();
-        vectorQueue.addFirst(new Vector2(0, 1));
-
-        ArrayList<FireTruck> truckArrayList = new ArrayList<>();
-
-        Alien alien = new Alien(0, 0, vectorQueue, 1);
-
-        alien.move(truckArrayList);
+        Alien alien = new Alien(1f, 1f, vectorList, 1f, masterFortress, new PathFinder(gameScreenMock), gameState);
+        alien.move(1f, new ArrayList<FireTruck>());
 
         assertEquals(new Vector2(0, 1), alien.getPosition());
     }
 
     @Test
+    public void alienMoveUpTest() {
+        when(gameScreenMock.isRoad(anyInt(), anyInt())).thenReturn(true);
+        List<Vector2> vectorList = new ArrayList<>();
+        vectorList.add(new Vector2(1, 10));     //Waypoint to move towards
+        GameState gameState = new GameState();
+        Fortress masterFortress = new Fortress(0f, 0f, FortressType.Clifford, gameState);
+
+        Alien alien = new Alien(1f, 1f, vectorList, 1f, masterFortress, new PathFinder(gameScreenMock), gameState);
+        alien.move(1f, new ArrayList<FireTruck>());
+
+        assertEquals(new Vector2(1, 2), alien.getPosition());
+    }
+
+
+    @Test
     public void alienMoveDownTest() {
-        Queue<Vector2> vectorQueue = new Queue<>();
-        vectorQueue.addFirst(new Vector2(0, -1));
+        when(gameScreenMock.isRoad(anyInt(), anyInt())).thenReturn(true);
+        List<Vector2> vectorList = new ArrayList<>();
+        vectorList.add(new Vector2(1, 0));     //Waypoint to move towards
+        GameState gameState = new GameState();
+        Fortress masterFortress = new Fortress(0f, 0f, FortressType.Clifford, gameState);
 
-        ArrayList<FireTruck> truckArrayList = new ArrayList<>();
+        Alien alien = new Alien(1f, 1f, vectorList, 1f, masterFortress, new PathFinder(gameScreenMock), gameState);
+        alien.move(1f, new ArrayList<FireTruck>());
 
-        Alien alien = new Alien(0, 0, vectorQueue, 1);
-
-        alien.move(truckArrayList);
-
-        assertEquals(new Vector2(0, -1), alien.getPosition());
+        assertEquals(new Vector2(1, 0), alien.getPosition());
     }
 
     @Test
     public void alienShouldCollideTest() {
-        ArrayList<FireTruck> truckArrayList = new ArrayList<>();
-        truckArrayList.add(new FireTruck(gameScreenMock, new Vector2(5, 5), Speed));
+        when(gameScreenMock.isRoad(2, 1)).thenReturn(false);
+        List<Vector2> vectorList = new ArrayList<>();
+        vectorList.add(new Vector2(3, 1));     //Waypoint to move towards
+        GameState gameState = new GameState();
+        Fortress masterFortress = new Fortress(0f, 0f, FortressType.Clifford, gameState);
 
-        Queue<Vector2> vectorQueue = new Queue<>();
-        vectorQueue.addFirst(new Vector2(5, 5));
+        Alien alien = new Alien(1f, 1f, vectorList, 1f, masterFortress, new PathFinder(gameScreenMock), gameState);
+        alien.move(1f, new ArrayList<FireTruck>());
 
-        Alien alien = new Alien(4, 5, vectorQueue , 1);
-
-        alien.move(truckArrayList);
-
-        assertEquals(new Vector2(4, 5), alien.getPosition());
+        assertNotEquals(new Vector2(2, 1), alien.getPosition());
     }
+    /**
+     * Helper function that returns true if an inte
+     */
 
-    @Test
-    public void alienShouldNotCollideTest() {
-        ArrayList<FireTruck> truckArrayList = new ArrayList<>();
-        truckArrayList.add(new FireTruck(gameScreenMock, new Vector2(5, 5), Speed));
-
-        Queue<Vector2> vectorQueue = new Queue<>();
-        vectorQueue.addFirst(new Vector2(4, 5));
-
-        Alien alien = new Alien(3, 5, vectorQueue , 1);
-
-        alien.move(truckArrayList);
-
-        assertEquals(new Vector2(4, 5), alien.getPosition());
-    }
 }
-*/
